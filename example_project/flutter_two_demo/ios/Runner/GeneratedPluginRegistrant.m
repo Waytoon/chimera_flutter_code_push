c5046bd067b620aba5a1f732bbe4c07b0ad05e6f
fang <457636876@qq.com>
2021-07-15 14:12:32 +0800

    no message

---
+++ example_project/flutter_two_demo/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<wtbase/WtbasePlugin.h>)
+#import <wtbase/WtbasePlugin.h>
+#else
+@import wtbase;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [WtbasePlugin registerWithRegistrar:[registry registrarForPlugin:@"WtbasePlugin"]];
+}
+
+@end
